c4af1b634bb4c717bf6ef5add0ca2b4362f81c20
vladimir <grun_gespenst@tut.by>
2021-03-21 12:17:23 +0300

    vk_bot_1

--- bot_vk.py
+++ bot_vk.py
@@ -1,8 +1,15 @@
-group_id = 203288690
+import vk_api
 from token_VK import _token
+
+group_id = 203288690
+
 class Bot:
     def __init__(self, group_id, _token):
-        pass
+        self.group_id = group_id
+        self.token = _token
+        self.vk = vk_api.VkApi(token=_token)
+        self.long_poller = vk_api.bot_L
+
     def run(self):
         pass
 
