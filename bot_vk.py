297afb14bd1a26a734f8a5ce07911b7506d3f81f
vladimir <grun_gespenst@tut.by>
2021-03-21 12:04:39 +0300

    Initial commit

---
+++ bot_vk.py
@@ -0,0 +1,11 @@
+group_id = 203288690
+from token_VK import _token
+class Bot:
+    def __init__(self, group_id, _token):
+        pass
+    def run(self):
+        pass
+
+if __name__ == 'main':
+    bot = Bot(group_id, _token)
+    bot.run()
