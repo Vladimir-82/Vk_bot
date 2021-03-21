16e1c8c102460693419b6ce2196d498032690066
vladimir <grun_gespenst@tut.by>
2021-03-21 19:30:28 +0300

    vk_bot_2

--- bot_vk.py
+++ bot_vk.py
@@ -1,5 +1,6 @@
 import vk_api
 from token_VK import _token
+import vk_api.bot_longpoll
 
 group_id = 203288690
 
@@ -8,11 +9,19 @@ class Bot:
         self.group_id = group_id
         self.token = _token
         self.vk = vk_api.VkApi(token=_token)
-        self.long_poller = vk_api.bot_L
+        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
 
     def run(self):
-        pass
+        for event in self.long_poller.listen():
+            print('Получено событие')
+            try:
+                self.on_event(event)
+            except Exception as err:
+                print(err)
 
-if __name__ == 'main':
+    def on_event(self, event):
+        print(event)
+
+if __name__ == '__main__':
     bot = Bot(group_id, _token)
     bot.run()
